e39448bf2f2b867163dda84cdc8fcd2de644ba80
Smurfa <smurfa-021@hotmail.com>
2013-11-29 10:14:31 +0100

    Removed memory leak check from main program.

--- Client/Source/clientProgram.cpp
+++ Client/Source/clientProgram.cpp
@@ -3,13 +3,8 @@
 
 #include <iostream>
 
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
-
 int main(int argc, char* argv[])
 {
-	_CrtSetDbgFlag ( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
 	BaseGameApp game;
 
 	try
