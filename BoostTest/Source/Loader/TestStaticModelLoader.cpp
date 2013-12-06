efd113799179dd796a60a32cf76bedcbb7e7ad73
Philip Andersson <philip.andersson.ha@live.se>
2013-12-06 11:48:46 +0100

    Animation loaded and stored
    Testing is done

--- BoostTest/Source/Loader/TestStaticModelLoader.cpp
+++ BoostTest/Source/Loader/TestStaticModelLoader.cpp
@@ -16,30 +16,16 @@ BOOST_AUTO_TEST_CASE(TestLoadingFile)
 {
 	ModelLoader loader;
 	bool test;
-	test = loader.loadFile("Source/Sample140.tx");
+	test = loader.loadFile("Source/Sample130.tx");
 	BOOST_CHECK_EQUAL(test, true);
 
 	std::vector<std::vector<ModelLoader::IndexDesc>> temp = loader.getIndices();
 
-	//BOOST_CHECK_EQUAL(temp.size(), 2);
-
-	//BOOST_CHECK_EQUAL(temp.at(0).at(0).m_MaterialID, "-blinn1");
+	BOOST_CHECK_EQUAL(temp.size(), loader.getNumberOfMaterial());
 
 	std::vector<DirectX::XMFLOAT3> tempVer = loader.getVertices();
 
-	//BOOST_CHECK_EQUAL(tempVer.size(), 8);
-
-	std::vector<DirectX::XMFLOAT3> tempNor = loader.getNormals();
-
-	//BOOST_CHECK_EQUAL(tempNor.size(), 24);
-
-	std::vector<DirectX::XMFLOAT3> tempTan = loader.getTangents();
-
-	//BOOST_CHECK_EQUAL(tempTan.size(), 36);
-
-	std::vector<DirectX::XMFLOAT2> tempUV = loader.getTextureCoords();
-
-	//BOOST_CHECK_EQUAL(tempUV.size(), 14);
+	BOOST_CHECK_EQUAL(tempVer.size(), loader.getNumberOfVertices());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
--- Graphics/Source/ModelLoader.h
+++ Graphics/Source/ModelLoader.h
@@ -9,6 +9,22 @@
 class ModelLoader
 {
 public:
+	struct KeyFrame
+	{
+		DirectX::XMFLOAT3 m_Trans;
+		DirectX::XMFLOAT4 m_Rot;
+		DirectX::XMFLOAT3 m_Scale;
+	};
+
+	struct Joint
+	{
+		std::string m_JointName;
+		int m_ID;
+		int m_Parent;
+		DirectX::XMFLOAT4X4 m_JointOffsetMatrix;
+		std::vector<KeyFrame> m_JointAnimation;
+	};
+
 	struct IndexDesc
 	{
 		std::string m_MaterialID;
@@ -26,31 +42,21 @@ public:
 		std::string m_SpecularMap;
 	};
 
-	struct KeyFrame
-	{
-		DirectX::XMFLOAT3 m_Trans;
-		DirectX::XMFLOAT3 m_Rot;
-		DirectX::XMFLOAT3 m_Scale;
-	};
-
 private:
-	int m_NumberOfMaterials;
+	
 	std::string m_MeshName;
-	int m_NumberOfVertices;
 	int m_NumberOfTriangles;
-	float m_Start;
-	int m_End,m_NumFrames;
-	std::vector<DirectX::XMFLOAT3> m_Vertices;
-	std::vector<DirectX::XMFLOAT3> m_Tangents;
-	std::vector<DirectX::XMFLOAT3> m_Normals;
-	std::vector<DirectX::XMFLOAT2> m_TextureCoord;
 	std::vector<IndexDesc> m_Indices;
+
+	float m_Start, m_End;
+	int m_NumberOfFrames, m_NumberOfVertices, m_NumberOfMaterials;
+	std::vector<DirectX::XMFLOAT3> m_Vertices, m_Normals, m_Tangents;
+	std::vector<DirectX::XMFLOAT2> m_TextureCoord;
 	std::vector<Material> m_Material;
 	std::vector<std::vector<IndexDesc>> m_IndexPerMaterial;
 	std::vector<std::pair<DirectX::XMFLOAT4, DirectX::XMFLOAT4>> m_WeightsList;
-	std::vector<std::pair<int, int>> m_JointHierarchy;
-	std::vector<DirectX::XMFLOAT4X4> m_JointOffsetMatrix;
-	std::vector<std::vector<KeyFrame>> m_JointAnimation;
+	std::vector<Joint> m_ListOfJoints;
+	
 public:
 	
 	/**
@@ -80,40 +86,91 @@ public:
 	 *
 	 * @returns a vector of vertices.
 	 */
-	std::vector<DirectX::XMFLOAT3> getVertices();
+	const std::vector<DirectX::XMFLOAT3>& getVertices();
 
 	/**
 	 * Returns the stored information about indices as a vector with Float3 values.
 	 *
 	 * @returns a vector of indices.
 	 */
-	std::vector<std::vector<IndexDesc>> getIndices();
+	const std::vector<std::vector<ModelLoader::IndexDesc>>& getIndices();
 
 	/**
 	 * Returns the stored information about the materials that are used by the model.
 	 *
 	 * @returns a vector of material structs.
 	 */
-	std::vector<Material> getMaterial();
+	const std::vector<ModelLoader::Material>& getMaterial();
 
 	/**
 	 * Returns the stored information about normal as a vector with Float3.
 	 *
 	 * @returns a vector of normals.
 	 */
-	std::vector<DirectX::XMFLOAT3> getNormals();
+	const std::vector<DirectX::XMFLOAT3>& getNormals();
 
 	/**
 	 * Returns the stored information about tangents as a vector with Float3. 
 	 *
 	 * @returns a vector of tangents.
 	 */
-	std::vector<DirectX::XMFLOAT3> getTangents();
+	const std::vector<DirectX::XMFLOAT3>& getTangents();
 
 	/**
 	 * Returns the stored information about uv coordinates as a vector with Float2. 
 	 *
 	 * @returns a vector of texture coords.
 	 */
-	std::vector<DirectX::XMFLOAT2> getTextureCoords();
+	const std::vector<DirectX::XMFLOAT2>& getTextureCoords();
+
+	/**
+	 * Returns the number of materials for the test. 
+	 *
+	 * @returns an int with the correct size of the vector.
+	 */
+	int getNumberOfMaterial();
+
+	/**
+	 * Returns the number of vertices in the vector.
+	 *
+	 * @returns an int with the correct size of the vector.
+	 */
+	int getNumberOfVertices();
+
+	/**
+	 * Returns a paired vector with information about weights and to what joint it is weighted.
+	 *
+	 * @returns a paired vector of weights and joints.
+	 */
+	const std::vector<std::pair<DirectX::XMFLOAT4, DirectX::XMFLOAT4>>& getWeightsList();
+	
+	/**
+	 * Returns a list of joint structs that includes information about animation and parents.
+	 *
+	 * @return a vector of Joint. 
+	 */
+	const std::vector<ModelLoader::Joint>& getListOfJoints();
+
+	/**
+	 * Returns the start number for the animation. 
+	 *
+	 * @returns a float with the start value
+	 */
+	float getAnimationStartValue();
+
+	/**
+	 * Returns the end number for the animation. 
+	 *
+	 * @returns a float with the end value
+	 */
+	float getAnimationEndValue();
+
+	/**
+	 * Returns the number of frames for the animation.
+	 *
+	 * @returns an in number of frames.
+	 */
+	int getNumberOfFrames();
+private:
+	void clearData();
 };
