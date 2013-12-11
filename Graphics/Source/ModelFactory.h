445abab43257119f56d1ead55e8d6a94d1047d02
Smurfa <smurfa-021@hotmail.com>
2013-12-11 16:40:03 +0100

    I don't know. AH-140

--- Graphics/Source/ModelFactory.h
+++ Graphics/Source/ModelFactory.h
@@ -2,31 +2,32 @@
 #include "Util.h"
 #include "WrapperFactory.h"
 #include "ModelDefinition.h"
+//#include <d3d11.h>
 
-using namespace DirectX;
+//using namespace DirectX;
 
 class ModelFactory
 {
 private:
-	struct Vertex
-	{
-		XMFLOAT4 position;
-		XMFLOAT3 normal;
-		XMFLOAT2 uv;
-		XMFLOAT3 tangent;
-		XMFLOAT3 binormal;
-		Vertex(){}
-		Vertex(XMFLOAT3 _position, XMFLOAT3 _normal, XMFLOAT2 _uv, XMFLOAT3 _tangent)
-		{
-			position = XMFLOAT4(_position.x, _position.y, _position.z, 1.0f);
-			normal = _normal;
-			uv = _uv;
-			tangent = _tangent;
+	//struct Vertex
+	//{
+	//	XMFLOAT4 position;
+	//	XMFLOAT3 normal;
+	//	XMFLOAT2 uv;
+	//	XMFLOAT3 tangent;
+	//	XMFLOAT3 binormal;
+	//	Vertex(){}
+	//	Vertex(XMFLOAT3 _position, XMFLOAT3 _normal, XMFLOAT2 _uv, XMFLOAT3 _tangent)
+	//	{
+	//		position = XMFLOAT4(_position.x, _position.y, _position.z, 1.0f);
+	//		normal = _normal;
+	//		uv = _uv;
+	//		tangent = _tangent;
 
-			//might be wrong
-			XMStoreFloat3(&binormal, XMVector3Cross(XMLoadFloat3(&tangent), XMLoadFloat3(&normal)));
-		}
-	};
+	//		//might be wrong
+	//		XMStoreFloat3(&binormal, XMVector3Cross(XMLoadFloat3(&tangent), XMLoadFloat3(&normal)));
+	//	}
+	//};
 
 	static ModelFactory *m_Instance;
 
