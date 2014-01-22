7fd2f4d1bfc5e53f79486655b5fe30ed9273347f
BuffBuff <Cronqvist_Fredrik@hotmail.com>
2014-01-22 16:45:47 +0100

    AH-170 added more function structure to the particle system

--- Graphics/Source/ParticleSystem.cpp
+++ Graphics/Source/ParticleSystem.cpp
@@ -3,6 +3,20 @@
 
 ParticleSystem::ParticleSystem(void)
 {
+	m_SysPosition			= DirectX::XMFLOAT4(0.f, 0.f, 0.f, 0.f);
+	m_SysType				= nullptr;
+
+	m_SysName				= nullptr;
+	m_TextureFilePath		= nullptr;
+	m_TextureU				= 0.f;
+	m_TextureV				= 0.f;
+	m_ParticleLife			= 0.f;
+
+	m_MaxParticles			= 0;
+	m_ParticleDeviation		= DirectX::XMFLOAT4(0.f, 0.f, 0.f, 0.f);
+	m_VelocityDeviation		= 0.f;
+	m_ParticlesPerSecound	= 0.f;
+
 }
 
 
@@ -10,7 +24,51 @@ ParticleSystem::~ParticleSystem(void)
 {
 }
 
-bool loadParticleSystemFromFile(const char* p_filename)
+bool ParticleSystem::loadParticleSystemFromFile(const char* p_filename)
 {
+	//use std::istream!
+	//#include<fstream>
+	//#include<sstream>
 	return false;
+}
+
+void ParticleSystem::init()
+{
+
+}
+
+void ParticleSystem::update(float p_DeltaTime, ID3D11DeviceContext* p_DeviceContext)
+{
+	killOldParticles();
+
+	emitNewParticles(p_DeltaTime);
+
+	updateParticles(p_DeltaTime);
+
+	updateBuffers(p_DeviceContext);
+}
+
+void ParticleSystem::emitNewParticles(float p_DeltaTime)
+{
+
+}
+
+void ParticleSystem::killOldParticles()
+{
+
+}
+
+void ParticleSystem::updateParticles(float p_DeltaTime)
+{
+
+}
+
+void ParticleSystem::updateBuffers(ID3D11DeviceContext* p_DeviceContext)
+{
+
+}
+
+void ParticleSystem::render()
+{
+
 }
--- Graphics/Source/ParticleSystem.h
+++ Graphics/Source/ParticleSystem.h
@@ -7,28 +7,52 @@
 
 struct Particle
 {
-	DirectX::XMFLOAT4 position;
+	DirectX::XMFLOAT4 startPosition; //pos from the syspos, in cm
+	DirectX::XMFLOAT4 startVelocity;
+	DirectX::XMFLOAT4 color;
+	float sizeX;
+	float sizeY;
 	float life;
 	float lifeMax;
+
+	Particle(DirectX::XMFLOAT3 p_startPosition, DirectX::XMFLOAT3 p_startVelocity, DirectX::XMFLOAT4 p_Color,
+		float p_SizeX, float p_SizeY, float p_Life, float p_LifeMax)
+	{
+		startPosition = DirectX::XMFLOAT4(p_startPosition.x, p_startPosition.y, p_startPosition.z, 1.0f);
+		startVelocity = DirectX::XMFLOAT4(p_startVelocity.x, p_startVelocity.y, p_startVelocity.z, 1.0f);
+		color = p_Color;
+		sizeX	= p_SizeX;
+		sizeY	= p_SizeY;
+		life	= p_Life;
+		lifeMax = p_LifeMax;
+	}
+};
+
+struct  VertexType
+{
+	//???
 };
 
 class ParticleSystem
 {
 private:
-	DirectX::XMFLOAT4	m_Position;
+	DirectX::XMFLOAT4	m_SysPosition; //world pos, in cm
 	std::string			m_SysType;
 
-	Buffer				m_Buffer;
-	Shader				m_Shader;
+	//Buffer				m_Buffer; //probably getting fixed out in the graphic class
+	//Shader				m_Shader; //probably getting fixed out in the graphic class
 
 	//Header
 	std::string			m_SysName;
+	std::string			m_TextureFilePath;
+	float				m_TextureU; 
+	float				m_TextureV;	
 	float				m_ParticleLife;
 
 	unsigned int		m_MaxParticles;
-	std::string			m_TextureFilePath;
-	DirectX::XMFLOAT4	m_Color;
-	
+	DirectX::XMFLOAT4	m_ParticleDeviation;
+	float				m_VelocityDeviation;
+	float				m_ParticlesPerSecound;
 
 public:
 	ParticleSystem(void);
@@ -36,11 +60,16 @@ public:
 
 	bool loadParticleSystemFromFile(const char* p_filename);
 
-	void update();
-	void emitNewParticles();
-	void killOldParticles();
+	void init();
+	void update(float p_DeltaTime, ID3D11DeviceContext* p_DeviceContext);
+
+	void render();
+	
 
 private:
-
+	void emitNewParticles(float p_DeltaTime);
+	void killOldParticles();
+	void updateParticles(float p_DeltaTime);
+	void updateBuffers(ID3D11DeviceContext* p_DeviceContext);
 };
 
