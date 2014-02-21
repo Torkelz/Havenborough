c3c807f6bd3e5c5086a32724ebae9760179f5714
Torkelz <and_re1@hotmail.com>
2014-02-21 16:31:29 +0100

    Add FMOD to copied dlls.

--- Client/CollectDlls.bat
+++ Client/CollectDlls.bat
@@ -5,3 +5,4 @@ copy ..\Physics\Bin\Physics.dll Bin\
 copy ..\Server\Bin\Server.exe Bin\
 copy "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\redist\x86\Microsoft.VC110.CRT\msvcr110.dll" Bin\
 copy "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\D3Dcompiler_46.dll" Bin\
+copy "C:\Program Files (x86)\FMOD SoundSystem\FMOD Programmers API Windows\api\fmodex.dll" Bin\
