0b5c77e4ca073892e3e8bdd8ea15203ab1626002
m0rch <matfri@live.se>
2014-02-25 13:24:19 +0100

    AH-400 The quest for the goldener path has commenced

--- Common/Source/Animation.cpp
+++ Common/Source/Animation.cpp
@@ -154,10 +154,7 @@ void Animation::checkFades()
 		// If the clip is supposed to fade out and is closing in on the end of it's duration, start playing the next clip in the queue.
 		if( (float)(m_Tracks[i].clip->m_End - m_Tracks[i].clip->m_FadeOutFrames - 1) < m_Tracks[i].currentFrame && m_Tracks[i].clip->m_FadeOut)
 		{
-			if(!playQueuedClip(i))
-			{
-				//m_Tracks[i].active = false;
-			}
+			playQueuedClip(i);
 		}
 	}
 }
