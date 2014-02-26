1f30a0f48100f837937440f8c4660e187e31d084
Smurfa <smurfa-021@hotmail.com>
2014-02-26 13:26:39 +0100

    New window style. AH-424

--- Havenborough Launcher/MainWindow.xaml.cs
+++ Havenborough Launcher/MainWindow.xaml.cs
@@ -14,13 +14,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml;
+using MahApps.Metro.Controls;
 
 namespace Havenborough_Launcher
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         private GameList gameList;
 
