051a135774ef51d4611e28cb5c388afe7095a79a
Torkelz <and_re1@hotmail.com>
2014-03-05 11:53:33 +0100

    AH-498_Spline removed unused methods in ActorFactory.

--- Common/Source/ActorFactory.cpp
+++ Common/Source/ActorFactory.cpp
@@ -101,24 +101,6 @@ Actor::ptr ActorFactory::createActor(const tinyxml2::XMLElement* p_Data, Actor::
 	return actor;
 }
 
-Actor::ptr ActorFactory::createBasicModel(const std::string& p_Model, Vector3 p_Position)
-{
-	tinyxml2::XMLPrinter printer;
-	printer.OpenElement("Object");
-	pushVector(printer, p_Position);
-	printer.OpenElement("Model");
-	printer.PushAttribute("Mesh", p_Model.c_str());
-	printer.CloseElement();
-	printer.CloseElement();
-
-	tinyxml2::XMLDocument doc;
-	doc.Parse(printer.CStr());
-
-	Actor::ptr actor = createActor(doc.FirstChildElement("Object"));
-
-	return actor;
-}
-
 void addEdge(tinyxml2::XMLPrinter& p_Printer, Vector3 p_Position, Vector3 p_Halfsize)
 {
 	p_Printer.OpenElement("AABBPhysics");
@@ -126,25 +108,6 @@ void addEdge(tinyxml2::XMLPrinter& p_Printer, Vector3 p_Position, Vector3 p_Half
 	pushVector(p_Printer, "Halfsize", p_Halfsize);
 	pushVector(p_Printer, "OffsetPosition", p_Position);
 	p_Printer.CloseElement();
-}
-
-Actor::ptr ActorFactory::createCollisionSphere(Vector3 p_Position, float p_Radius)
-{
-	tinyxml2::XMLPrinter printer;
-	printer.OpenElement("Object");
-	pushVector(printer, p_Position);
-	printer.OpenElement("SpherePhysics");
-	printer.PushAttribute("Radius", p_Radius);
-	pushVector(printer, "Position", p_Position);
-	printer.CloseElement();
-	printer.CloseElement();
-
-	tinyxml2::XMLDocument doc;
-	doc.Parse(printer.CStr());
-
-	Actor::ptr actor = createActor(doc.FirstChildElement("Object"));
-
-	return actor;
 }
 
 Actor::ptr ActorFactory::createCheckPointActor(Vector3 p_Position, Vector3 p_Scale)
@@ -295,22 +258,6 @@ Actor::ptr ActorFactory::createPointLight(Vector3 p_Position, float p_Range, Vec
 	return actor;
 }
 
-Actor::ptr ActorFactory::createCheckPointArrow()
-{
-	tinyxml2::XMLPrinter printer;
-	printer.OpenElement("Object");
-	printer.OpenElement("Mesh");
-	pushVector(printer, "Scale", Vector3(1.0f, 1.0f, 1.0f));
-	pushVector(printer, "ColorTone", Vector3(1.0f, 1.0f, 1.0f));
-	printer.CloseElement();
-	printer.CloseElement();
-
-	tinyxml2::XMLDocument doc;
-	doc.Parse(printer.CStr());
-
-	return createActor(doc.FirstChildElement("Object"));
-}
-
 Actor::ptr ActorFactory::createParticles( Vector3 p_Position, const std::string& p_Effect )
 {
 	tinyxml2::XMLPrinter printer;
@@ -342,26 +289,6 @@ Actor::ptr ActorFactory::createParticles( Vector3 p_Position, const std::string&
 	doc.Parse(printer.CStr());
 
 	return createActor(doc.FirstChildElement("Object"));
-}
-
-Actor::ptr ActorFactory::createBoxWithOBB(Vector3 p_Position, Vector3 p_Halfsize, Vector3 p_Rotation)
-{
-	tinyxml2::XMLPrinter printer;
-	printer.OpenElement("Object");
-	pushVector(printer, p_Position);
-	pushRotation(printer, p_Rotation);
-	printer.OpenElement("OBBPhysics");
-	pushVector(printer, "Halfsize", p_Halfsize);
-	pushVector(printer, "Position", p_Position);
-	printer.CloseElement();
-	printer.CloseElement();
-
-	tinyxml2::XMLDocument doc;
-	doc.Parse(printer.CStr());
-
-	Actor::ptr actor = createActor(doc.FirstChildElement("Object"));
-
-	return actor;
 }
 
 Actor::ptr ActorFactory::createFlyingCamera(Vector3 p_Position)
