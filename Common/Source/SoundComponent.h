df3e23f6bbcc0ba88a80a3b7bef50f1bf1b8b73d
Philip Andersson <philip.andersson.ha@live.se>
2014-03-13 16:08:42 +0100

    AH-575 Small changes

--- Common/Source/SoundComponent.h
+++ Common/Source/SoundComponent.h
@@ -1,47 +1,45 @@
 #include "Components.h"
 #include "Logger.h"
-#include "ISound.h"
 
 class SoundComponent : public SoundInterface
 {
 private:
 	Vector3 m_Position;
 	float m_MinDistance;
-	const char* m_SoundID;
-	const char* m_FilePath;
-	ISound* m_Sound;
+	std::string m_SoundID;
+	std::string m_FilePath;
 public:
 
 	~SoundComponent() override
 	{
-
+		
 	}
 
 	void initialize(const tinyxml2::XMLElement* p_Data) override
 	{
 		m_MinDistance = 10.0f;
 		m_Position = Vector3(0,0,0);
-		m_SoundID = p_Data->GetText();
+		m_SoundID = p_Data->Attribute("SoundID");
+		m_FilePath = p_Data->Attribute("FilePath");
 		p_Data->QueryAttribute("MinDistance", &m_MinDistance);
-		queryVector(p_Data->FirstChildElement("Position"), m_Position);
 	}
 	
 	void postInit() override
 	{
-		m_Sound->load3DSound(m_SoundID, m_FilePath, m_MinDistance);
+		m_Sound->load3DSound(m_SoundID.c_str(), m_FilePath.c_str(), m_MinDistance); //Event baserat
 	}
 
 	void serialize(tinyxml2::XMLPrinter& p_Printer) const override
 	{
 		p_Printer.OpenElement("3DSound");
-		p_Printer.PushText(m_SoundID);
+		p_Printer.PushAttribute("SoundID", m_SoundID.c_str());
+		p_Printer.PushAttribute("FilePath", m_FilePath.c_str());
 		p_Printer.PushAttribute("MinDistance", m_MinDistance);
-		pushVector(p_Printer, "Position", m_Position);
 		p_Printer.CloseElement();
 	}
 
 	void onUpdate(float p_DeltaTime) override
 	{
-		
+		m_Sound->onFrameSound(m_SoundID.c_str(), &m_Owner->getPosition(), NULL); //Event baserat
 	}
 };
